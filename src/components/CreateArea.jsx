049d8003e3cd71d21389929093beb8186247a0d1
guieiji <47559146+guieiji@users.noreply.github.com>
2022-07-14 19:02:24 -0300

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [clicked, setClicked] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function handleClick() {
+    setClicked(true);
+  }
+
+  function handleHoverOut() {
+    setClicked(false);
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form onMouseLeave={handleHoverOut} className="create-note">
+        <input
+          name="title"
+          type={clicked ? null : "hidden"}
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={handleClick}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={clicked ? "3" : "1"}
+        />
+
+        <Zoom in={clicked ? true : false}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
